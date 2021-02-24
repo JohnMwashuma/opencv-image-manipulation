1879d5f67213fd0c105b59b70a2f57ee0d061f8b
John Mwashuma <jmwashuma@live.com>
2021-02-24 21:41:34 +0300

    Add logic for reading images

---
+++ reading_images.py
@@ -0,0 +1,9 @@
+import cv2 as cv
+
+# Reading image
+img = cv.imread('Photos/cat.jpg')
+
+cv.imshow('Cat', img)
+
+# Keyboard binding function, waits for an infinite amount of time for the keyboad to be pressed
+cv.waitKey(0)
