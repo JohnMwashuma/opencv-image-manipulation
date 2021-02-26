05e2447168e29725978168ea7f07d201fb5c176e
Clifonyalo <53649207+Clifonyalo@users.noreply.github.com>
2021-02-26 18:01:43 +0300

    Add files via upload

---
+++ webcam.py
@@ -0,0 +1,15 @@
+  
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0) #No of webcam. 0 access 1 camera; 1 access 2 cameras
+
+while True:
+    ret, frame = cap.read() #Captures the image and stores in an array. Camera won't be used if another App is using it. ret returns false if there's an issue
+    cv2.imshow('CLIFF', frame)
+
+    if cv2.waitKey(1) == ord('c'): #Waits upto 1 milisecond 
+        break
+
+cap.release()
+cv2.destroyAllWindows()
