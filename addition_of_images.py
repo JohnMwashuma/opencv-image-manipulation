d38a813d2253f5a7b02227f58845f183be90eed6
ebbie-awino <ebbieawino@gmail.com>
2021-02-26 13:43:41 +0300

    Create addition_of_images.py

---
+++ addition_of_images.py
@@ -0,0 +1,14 @@
+import cv2 
+
+image1 = cv2.imread('Photos/input1.jpg') 
+image2 = cv2.imread('Photos/.jpg') 
+
+weightedSum = cv2.addWeighted(image1, 0.5, image2, 0.4, 0) 
+
+cv2.imshow('Image 1', image1) 
+cv2.imshow('Image 2', image2) 
+cv2.imshow('Weighted Image', weightedSum) 
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows() 
