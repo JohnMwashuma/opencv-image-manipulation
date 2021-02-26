311dd7a3ad08a3473f9c920e9daa4a96be01ee4a
ebbie-awino <ebbieawino@gmail.com>
2021-02-26 13:50:24 +0300

    Update subtraction_of_images.py

--- subtraction_of_images.py
+++ subtraction_of_images.py
@@ -5,6 +5,8 @@ image2 = cv2.imread('Photos/circle.jpg')
 
 sub = cv2.subtract(image1, image2) 
 
+cv2.imshow('Image 1', image1) 
+cv2.imshow('Image 2', image2) 
 cv2.imshow('Subtracted Image', sub) 
 
 cv2.waitKey(0)
