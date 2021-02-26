59e5ff754b45d2c8da0e1f8db1e8936e91c8b0f6
ebbie-awino <ebbieawino@gmail.com>
2021-02-26 13:49:22 +0300

    Create subtraction_of_images.py

---
+++ subtraction_of_images.py
@@ -0,0 +1,11 @@
+import cv2 
+	
+image1 = cv2.imread('Photos/star.jpg')
+image2 = cv2.imread('Photos/circle.jpg') 
+
+sub = cv2.subtract(image1, image2) 
+
+cv2.imshow('Subtracted Image', sub) 
+
+cv2.waitKey(0)
+cv2.destroyAllWindows() 
