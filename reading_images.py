599294f5721bfc85a75d6f6a8b5cdfeb8a7b07b7
ebbie-awino <ebbieawino@gmail.com>
2021-02-26 14:07:41 +0300

    Update reading_images.py

--- reading_images.py
+++ reading_images.py
@@ -3,7 +3,11 @@ import cv2 as cv
 # Reading image
 img = cv.imread('Photos/cat.jpg')
 
+#reading the image in grayscale
+img2 = cv.imread('Photos/cat.jpg', 0)
+
 cv.imshow('Cat', img)
+cv.imshow('Grayscale Cat', img2)
 
 # Keyboard binding function, waits for an infinite amount of time for the keyboad to be pressed
 cv.waitKey(0)
